eee885d12ff194b63232fb5e20883212ddcc8123
havveFn <46079709+havveFn@users.noreply.github.com>
2019-08-13 00:05:32 +0900

    Feature: CORSまわりのheaderを追加した

--- src/history.ts
+++ src/history.ts
@@ -20,6 +20,9 @@ export const handler = async (event: APIGatewayProxyEvent) => {
     .then(({ data }) => {
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
         body: JSON.stringify(data),
       }
     })
